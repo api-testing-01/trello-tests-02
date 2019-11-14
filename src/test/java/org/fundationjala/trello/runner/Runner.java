e11e5656b63ae61e268d979ff53312a2618d5c38
lfc <lufer.lfcc@gmail.com>
2019-11-13 21:10:31 -0800

    - adding travis - adding checkstyle - solving issues styles and run test issues

--- src/test/java/org/fundationjala/trello/runner/Runner.java
+++ src/test/java/org/fundationjala/trello/runner/Runner.java
@@ -1,10 +1,10 @@
-package org.fundationjala.tello.runner;
+package org.fundationjala.trello.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        glue = {"org.fundationjala.tello"},
+        glue = {"org.fundationjala.trello"},
         features = "src/test/resources/feature",
         plugin = "pretty"
 )
